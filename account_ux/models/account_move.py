0532370f521d7edce7aa68f0790b971e9655d82c
Juan Pablo Levy <operaciones@indasoge.com>
2022-11-30 15:49:31 -0300

    Fix correction in account_move.py from account_ux from OCA
    Fix correction account_move.py from account_ux from where unlink has been uncommented
    
    Fixes-Bug: Fix error when changing product in PO

--- account_ux/models/account_move.py
+++ account_ux/models/account_move.py
@@ -192,11 +192,11 @@ class AccountMove(models.Model):
             raise ValidationError(_(
                 'You can create sales/purchase invoices exclusively in the respective sales/purchase journals'))
 
-    def unlink(self):
-        """ If we delete a journal entry that is related to a reconcile line then we need to clean the statement line
-        in order to be able to reconcile in the future (clean up the move_name field)."""
-        self.mapped('line_ids.statement_line_id').write({'move_name': False})
-        return super().unlink()
+    # def unlink(self):
+    #     """ If we delete a journal entry that is related to a reconcile line then we need to clean the statement line
+    #     in order to be able to reconcile in the future (clean up the move_name field)."""
+    #     self.mapped('line_ids.statement_line_id').write({'move_name': False})
+    #     return super().unlink()
 
     def _recompute_tax_lines(self, recompute_tax_base_amount=False):
         """ Odoo recomputa todos los impuestos cada vez que hay un cambio en la factura, esto trae dos problemas:
